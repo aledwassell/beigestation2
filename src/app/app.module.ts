2e17f2e400c5d155c40a1cd95ec304d22f34df72
aledwassell <aledwassell@googlemail.com>
2019-06-16 09:17:42 +0100

    6 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import {
   MatButtonModule,
